af33106ef458f44cffa2f03adb0be3e31f1047f9
aschmi34 <aschmi34@students.towson.edu>
2021-12-08 01:49:48 -0500

    inserted 3 employees

--- backend/src/main/resources/db/migration/V1.2__employees.sql
+++ backend/src/main/resources/db/migration/V1.2__employees.sql
@@ -14,3 +14,59 @@
 --                         resumeID
 --                    );
 
+
+--Sally Jones
+
+insert into employee (empID, firstName, lastName, middleI, ssn, address1, address2, city, state,
+                      postalCode, counrty, hireDate, termDate, partner, resumeID)
+            values (
+                        1, 'Sally', 'Jones', 'R',
+                        '211225544',
+                        '20 Jersey Court',
+                        'Apt 6',
+                        'Timonium', 'MD',
+                        '21093',
+                        'US',
+                        '12/01/2021',
+                        null,
+                        false,
+                        1
+                   );
+
+
+-- --Chris Whine
+
+insert into employee (empID, firstName, lastName, middleI, ssn, address1, address2, city, state,
+                      postalCode, counrty, hireDate, termDate, partner, resumeID)
+values (
+           2, 'Chris', 'Whine', 'A',
+           '222222222',
+           '20 Wayne Street',
+           null,
+           'Timonium', 'MD',
+           '21093',
+           'US',
+           '12/01/2021',
+           null,
+           false,
+           2
+       );
+
+-- --Keith Taylor
+
+insert into employee (empID, firstName, lastName, middleI, ssn, address1, address2, city, state,
+                      postalCode, counrty, hireDate, termDate, partner, resumeID)
+values (
+           3, 'Keith', 'Taylor', 'C',
+           '333333333',
+           '30 Belmor Drive',
+           'Apt 19',
+           'Timonium', 'MD',
+           '21093',
+           'US',
+           '12/01/2021',
+           null,
+           false,
+           3
+       );
+
